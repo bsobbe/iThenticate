7303c93a2ca439bf835bc8aa979df058ea6d7ea9
Sobhan <s.bagheri@bahar.network>
2017-03-28 11:15:16 +0430

    Private properties has been added for authentication - Setter and getter methods has been defined for each property

--- Ithenticate.php
+++ Ithenticate.php
@@ -4,5 +4,54 @@ namespace bsobbe\ithenticate;
 
 class Ithenticate
 {
+    private $username;
+    private $password;
+    private $sid;
 
+    public function __construct($username, $password)
+    {
+
+    }
+
+    public function setUsername($username)
+    {
+        if (property_exists($this, 'username')) {
+            $this->username = $username;
+        }
+    }
+
+    public function setPassword($password)
+    {
+        if (property_exists($this, 'password')) {
+            $this->password = $password;
+        }
+    }
+
+    public function setSid($sid)
+    {
+        if (property_exists($this, "sid")) {
+            $this->sid = $sid;
+        }
+    }
+
+    public function getUsername()
+    {
+        if (property_exists($this, 'username')) {
+            return $this->username;
+        }
+    }
+
+    public function getPassword()
+    {
+        if (property_exists($this, 'password')) {
+            return $this->password;
+        }
+    }
+
+    public function getSid()
+    {
+        if (property_exists($this, 'sid')) {
+            return $this->sid;
+        }
+    }
 }
